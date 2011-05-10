08a6af9dfe4e6f43d5ced6b4101cc2e272dc7a49
Aaron Severs <asevers@gmail.com>
2011-05-10 09:10:31 -0700

    another attempt to fix android field not blurring on login

--- src/views.js
+++ src/views.js
@@ -47,13 +47,18 @@ function setUpViews() {
             keyup: function(fld, e){
                 if (e.browserEvent.keyCode == 13) {
                 e.stopEvent();
+                if (Ext.is.Android) {
+                    setTimeout(function() {Ext.getCmp('passwordField').blur();},300); // Android has trouble blurring fields when the keyboard is half open
+                }
+                else {
+                    fld.fieldEl.dom.blur();
+                }
                 Ext.CTCT.login(userNameField.getValue(),passwordField.getValue(),function(){
                     refreshEventsList();
                 });
                 //also clear the fields so if they come back to the login screen they have a clean slate
                 userNameField.reset();
                 passwordField.reset();
-                fld.fieldEl.dom.blur();
                 }
             }
         }
@@ -66,6 +71,11 @@ function setUpViews() {
         handler: function() {
             if (Ext.is.Android) {
                 window.KeyBoard.hideKeyBoard(); // stupid Android... needs me to tell it to close the keyboard!
+                setTimeout(function() {Ext.getCmp('passwordField').blur();Ext.getCmp('userNameField').blur();},300); // Android has trouble blurring fields when the keyboard is half open
+            }
+            else {
+                passwordField.blur();
+                userNameField.blur();
             }
             //login with callback to get the events list
             Ext.CTCT.login(userNameField.getValue(),passwordField.getValue(),function() {
@@ -73,8 +83,6 @@ function setUpViews() {
             });
             userNameField.reset();
             passwordField.reset();
-            passwordField.blur();
-            userNameField.blur();
         }
     });
 
