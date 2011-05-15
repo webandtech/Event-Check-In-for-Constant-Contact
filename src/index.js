8078363678079acbeb789c21eba6b606468e8cb4
Aaron Severs <asevers@gmail.com>
2011-05-15 11:10:35 -0700

    Edited src/index.js via GitHub

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ function onDeviceReady() {
 }
 
 function autoLogin() {
-    var username = Ext.CTCT.init('https://api.constantcontact.com', '7be84572-2423-47d0-b62d-4662eaa04c18');
+    var username = Ext.CTCT.init('https://api.constantcontact.com', 'YOUR API KEY HERE');
     if (username != false) {
         refreshEventsList();
     }
